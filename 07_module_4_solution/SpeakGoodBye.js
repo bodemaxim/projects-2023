6d46e20d313164a4c68011ed48fe296523e09d2c
bodemaxim <bodemaxim@gmail.com>
2023-02-26 19:24:42 +0300

    two projects added

--- 07_module_4_solution/SpeakGoodBye.js
+++
@@ -1,9 +0,0 @@
-(function (window) {
-  const speakWord = "Good Bye";
-  const byeSpeaker = {
-    speak: function (name) {
-      console.log(speakWord + " " + name);
-    }
-  };
-  window.byeSpeaker = byeSpeaker;
-})(window);
